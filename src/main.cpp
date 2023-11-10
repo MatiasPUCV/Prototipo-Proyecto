071e089f40b629fc52a3552260cacbe6d177a295
MatiasPUCV <142541831+MatiasPUCV@users.noreply.github.com>
2023-11-10 20:22:52 -0300

    Added example in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,54 @@
 #include <stdio.h>
 #include <SDL2/SDL.h>
+#include <SDL_image.h>
 
-int main(int argc, char* args[])
-{
+#include "graphics.h"
+
+int main(int argc, char* args[]) {
+    
     // Inicializa SDL
-    if (SDL_Init(SDL_INIT_EVERYTHING) < 0)
-    {
-        printf("Error\n");
+    if (SDL_Init(SDL_INIT_EVERYTHING) < 0) {
+        printf("SDL2 no se pudo inicializar. %s\n", SDL_GetError());
     }
 
-    /*
+    if (IMG_Init(IMG_INIT_PNG) != IMG_INIT_PNG) {
+        printf("SDL_image no se pudo inicializar. %s\n", IMG_GetError());
+    }
+
+    RenderWindow window("Ventana", 800, 600);
+    Texture* texture = window.load_texture("assets/pucv.png");
+
+    Sprite sprite;
+    sprite.set_position(280, 200);
+    sprite.set_texture(texture);
+
+    SDL_Event event;
+    while (window.is_open())
+    {
+        while(SDL_PollEvent(&event))
+        {
+            switch (event.type)
+            {
+            case SDL_QUIT:
+                window.close();
+                break;
+
+            }
+        }
+
+        /* Actualizar */
+        sprite.rotate(3);
+        sprite.scale(1.005);
+        
+        /* Dibujar */
+        window.draw(sprite);
+
+        window.display();
+        window.clear();
+    }
+
+    SDL_DestroyTexture(texture);
     
-        Codigo aqui
-
-    */ 
-
     // Finaliza SDL
     SDL_Quit();
 
