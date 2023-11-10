0b2309552992a839150c269cfdf2d0daf1c41925
MatiasPUCV <142541831+MatiasPUCV@users.noreply.github.com>
2023-11-10 19:40:19 -0300

    Added basic sprite rendering

--- include/graphics/render_window.h
+++ include/graphics/render_window.h
@@ -1,9 +1,14 @@
 #ifndef RENDER_WINDOW_H
 #define RENDER_WINDOW_H
 
+#include "graphics/drawable.h"
+#include "graphics/texture.h"
+
 #include <SDL2/SDL.h>
 
 class RenderWindow {
+    friend Drawable;
+
 public:
 
     RenderWindow(const char* title, int width, int height);
@@ -22,7 +27,13 @@ public:
     void clear();
 
     /* Dibuja un objeto */
+    void draw(const Drawable& drawable) const;
+    Texture* load_texture(const char* filename);
+    SDL_Renderer* get_renderer() const { return this->renderer; }
 
+private:
+
+    SDL_Renderer* renderer;
     SDL_Window* window;
 
     bool is_closed;
